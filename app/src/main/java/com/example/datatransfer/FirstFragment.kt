b00e6adc3dab6cfa1779edadd9cc7d3a2c40ca33
dkchoi <ehdqkwk92@gmail.com>
2021-05-27 22:12:17 +0900

    data 공유를 위한 viewmodel 생성

--- app/src/main/java/com/example/datatransfer/FirstFragment.kt
+++ app/src/main/java/com/example/datatransfer/FirstFragment.kt
@@ -6,14 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.datatransfer.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment(R.layout.fragment_first) {
     lateinit var binding: FragmentFirstBinding
+
+    val mainViewModel by activityViewModels<MainViewModel>() // activity의 lifeCycle에 맞는 viewModel 생성
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.button.setOnClickListener {
+            mainViewModel.data = "Hello"
             findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
         }
     }
--- app/src/main/java/com/example/datatransfer/SecondFragment.kt
+++ app/src/main/java/com/example/datatransfer/SecondFragment.kt
@@ -5,15 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.datatransfer.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment(R.layout.fragment_second) {
     lateinit var binding: FragmentSecondBinding
+    val mainViewModel by activityViewModels<MainViewModel>() // activity의 lifeCycle에 맞는 viewModel 생성
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Toast.makeText(requireContext(), mainViewModel.data, Toast.LENGTH_SHORT).show()
         binding.button.setOnClickListener {
             findNavController().navigate(R.id.action_secondFragment_to_firstFragment)
         }
