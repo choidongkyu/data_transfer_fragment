8c860e099d51f2857f6ec1923335fda5a202b47c
dkchoi <ehdqkwk92@gmail.com>
2021-05-27 22:33:44 +0900

    fragment Result api 사용하여 fragment간 데이터 전달

--- app/src/main/java/com/example/datatransfer/FirstFragment.kt
+++ app/src/main/java/com/example/datatransfer/FirstFragment.kt
@@ -1,13 +1,12 @@
 package com.example.datatransfer
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.*
 import androidx.navigation.fragment.findNavController
 import com.example.datatransfer.databinding.FragmentFirstBinding
 
@@ -19,7 +18,9 @@ class FirstFragment : Fragment(R.layout.fragment_first) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.button.setOnClickListener {
-            mainViewModel.data = "Hello"
+
+            setFragmentResult("requestKey", bundleOf("data" to "hello"))
+
             findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
         }
     }
--- app/src/main/java/com/example/datatransfer/SecondFragment.kt
+++ app/src/main/java/com/example/datatransfer/SecondFragment.kt
@@ -8,16 +8,21 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
 import com.example.datatransfer.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment(R.layout.fragment_second) {
     lateinit var binding: FragmentSecondBinding
-    val mainViewModel by activityViewModels<MainViewModel>() // activity의 lifeCycle에 맞는 viewModel 생성
+    //val mainViewModel by activityViewModels<MainViewModel>() // activity의 lifeCycle에 맞는 viewModel 생성
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Toast.makeText(requireContext(), mainViewModel.data, Toast.LENGTH_SHORT).show()
+        setFragmentResultListener("requestKey") { requestKey, bundle ->
+            val result = bundle.getString("data", "")
+            Toast.makeText(requireContext(), result, Toast.LENGTH_SHORT).show()
+        }
+
         binding.button.setOnClickListener {
             findNavController().navigate(R.id.action_secondFragment_to_firstFragment)
         }
@@ -28,7 +33,8 @@ class SecondFragment : Fragment(R.layout.fragment_second) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_second, container, false)
+        binding =
+            DataBindingUtil.inflate(layoutInflater, R.layout.fragment_second, container, false)
         return binding.root
     }
 }
