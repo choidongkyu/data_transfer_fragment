34e8cae6ba9a34501d1edf4e04e793bb9af9cdb5
dkchoi <ehdqkwk92@gmail.com>
2021-05-30 23:18:45 +0900

    양방향으로 데이터 전달되도록 수정

--- app/src/main/java/com/example/datatransfer/FirstFragment.kt
+++ app/src/main/java/com/example/datatransfer/FirstFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.*
@@ -17,10 +18,12 @@ class FirstFragment : Fragment(R.layout.fragment_first) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setFragmentResultListener("requestKey") { requestKey, bundle ->
+            val result = bundle.getString("data", "")
+            Toast.makeText(requireContext(), result, Toast.LENGTH_SHORT).show()
+        }
         binding.button.setOnClickListener {
-
             setFragmentResult("requestKey", bundleOf("data" to "hello"))
-
             findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
         }
     }
--- app/src/main/java/com/example/datatransfer/SecondFragment.kt
+++ app/src/main/java/com/example/datatransfer/SecondFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
 import com.example.datatransfer.databinding.FragmentSecondBinding
@@ -24,6 +26,7 @@ class SecondFragment : Fragment(R.layout.fragment_second) {
         }
 
         binding.button.setOnClickListener {
+            setFragmentResult("requestKey", bundleOf("data" to "hello"))
             findNavController().navigate(R.id.action_secondFragment_to_firstFragment)
         }
     }
